36f45c90c9a5eb503a789f63b6aa237f69797608
Elson Felipe de Oliveira Urbano <elson_felipe@hotmail.com>
2023-07-22 11:04:07 -0300

    estudos pausados nas aulas de funções

---
+++ Scripts/exercicios.js
@@ -0,0 +1,91 @@
+// 1. Declare uma variavel de nome weight
+
+//let weight
+
+// 2. Que tipo de dado é a variável acima ?
+
+//console.log(typeof weight) //undefined
+
+/*
+   3. Declare uma variável e atribua valores para cada um dos dados:
+    * name: String
+    * age: Number (interger)
+    * stars: Number (float)
+    * isSubscribed: Boolean
+*/
+
+/* let name = 'Elson'
+let age = 29
+let stars = 4.5
+let isSubscribed = false */
+
+/*
+   4. A variável student abaixo é de que tipo de dado?
+
+   //object
+
+   4.1 Atribua a ela as mesmas propriedades e valores do exercício 3.
+
+  let student = {
+  name: 'Elson',
+  age: 29,
+  stars: 4.5,
+  isSubscribed: false
+}
+
+   4.2 Mostre no console a seguinte mensagem:
+
+      <name> de idade <age> pesa <weight> kg.
+
+      Atenção, substitua <name> <age> e <weight> pelos valores de cada propriedade do objeto
+*/
+
+/* let student = {};
+//console.log(typeof student)
+
+student = {
+  name: 'Elson',
+  age: 29,
+  weight: 94.5,
+  isSubscribed: false
+} */
+
+//console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg`)
+
+
+//5. Declare uma variável do tipo Array de nome students e atribua a ela nenhum valor, ou seja, somete o Array vazio 
+
+/* let students = [] */
+
+//6. Reatribua valor para a variável acima, colocando dentro dela o objeto student da questão 4. (Não copiar e colar o objeto, mas usar o objeto criado e inserir ele no Array)
+
+/* students = [student] */
+
+//7. Coloque no console o valor da posição zero do Array acima
+
+/* console.log(students[0]) */
+
+//8. Crie um novo student e coloque na posição 1 do Array students
+
+/* let student1 = {
+  name: 'João',
+  age: 18,
+  weight: 75.4,
+  isSubscribed: true
+}
+
+students = student1
+
+console.log(students)
+ */
+/*
+   9. Sem rodar o código responda qual é a resposta do código abaixo e porque? Após a sua resposta, rode o código e veja se acertou.
+
+   console.log(a)
+   var a = 1
+
+   vai dar undefined pois a variavel com var é global
+*/
+
+console.log(a)
+var a = 1
---
+++ Scripts/rev-variaveis.js
@@ -0,0 +1,27 @@
+// Variáveis e tipos de dados
+// declaração or declaration
+var name
+
+// assignment or atribuição de valores
+name = "Mike"
+
+// que tipo de dado foi colocado na variável
+//console.log(typeof name)
+
+/* let age = 20
+let isHuman = true */
+let age, isHuman // agrupamento de declarações
+
+age = 34
+isHuman = true
+
+// multiplos argumentos na função
+//console.log(name, age, isHuman)
+
+// escrita de texto + variáveis
+
+// concatenando valores
+//console.log('O ' + name +' tem ' + age + ' anos.')
+
+// interpolando valores com template literals ou template string
+console.log(`O ${name} tem ${age} anos.`)

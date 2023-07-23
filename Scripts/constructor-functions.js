6780012927841f40739158483f26473333a16968
Elson Felipe de Oliveira Urbano <elson_felipe@hotmail.com>
2023-07-23 11:02:15 -0300

    concluídas as aulas sobre funções

---
+++ Scripts/constructor-functions.js
@@ -0,0 +1,23 @@
+/*
+    function constructor
+
+    * expressão new
+    * criar um novo objeto
+    * this keyword
+  
+*/
+
+let date = new Date("2023-07-23")
+
+console.log(date)
+
+/* function Person(name) {
+  this.name = name
+  this.walk = function() {
+    return this.name + " está andando"
+  }
+}
+const mayk = new Person("Elson")
+const joao = new Person("João")
+console.log(mayk.walk())
+console.log(joao.walk()) */
